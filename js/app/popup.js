07796c2898b6eac5029e716ea2d8bfd9a8f99c98
Chris <christopherdieckhaus@gmail.com>
2015-05-31 14:47:03 -0400

    Able to add current page to list. Need way to store

---
+++ js/app/popup.js
@@ -0,0 +1,29 @@
+myApp.controller("PageController", function($scope){
+  $scope.message = "Hello";
+
+  chrome.tabs.query({'active': true},
+  function(tabs){
+    if (tabs.length > 0){
+      $scope.title = tabs[0].title;
+      $scope.url = tabs[0].url;
+      $scope.savedLinks = [];
+
+      $scope.saveLink = function(){
+        if ($scope.savedLinks.indexOf($scope.url) !== -1){
+          alert("Already in list");
+        }else{
+          $scope.savedLinks.push($scope.url);
+        }
+      };
+
+      $scope.removeLink = function(index){
+        $scope.savedLinks.splice(index, 1);
+      };
+
+      chrome.tabs.sendMessage(tabs[0].id, {'action': 'PageInfo'}, function(response){
+        $scope.pageInfos = response;
+        $scope.$apply();
+      });
+    }
+  });
+});
